e770a285efd2ad200a8628caf2c94285a31402bd
PlayContinueX <playcontinue10@gmail.com>
2022-11-23 14:10:33 +0900

    firstCommit

---
+++ android/app/src/main/kotlin/com/playcontinuex/taxdb_loader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.playcontinuex.taxdb_loader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const MainApp(),
+    );
+  }
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white);
+  }
+}
